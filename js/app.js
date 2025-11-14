3b28aae81333d6fa054b3ab355e63dafc7a1e41e
Leonel Cordero <leonelrcordero22@gmail.com>
2025-11-14 17:42:59 -0300

    Se agregaron estilos al login y se realizo la logica correspondiente

--- js/app.js
+++ js/app.js
@@ -2,22 +2,16 @@ const user = {
   username: 'leonel',
   password: 'admin',
 };
+const loginForm = document.getElementById('form-container');
 
-function Login() {
-  const loginForm = document.getElementById('login-form');
-  loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const userLogin = document.getElementById('user-input').value;
-    const passLogin = document.getElementById('password-input').value;
-
-    if (user.username == userLogin && user.password == passLogin) {
-      alertLogin();
-    } else {
-      alert('Datos incorrectos');
-    }
-  });
-}
-
-function alertLogin() {
-  alert('Incio de sesion correcto');
-}
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const userLogin = document.getElementById('user-input').value;
+  const passLogin = document.getElementById('password-input').value;
+  if (user.username == userLogin && user.password == passLogin) {
+    alert('Datos correctos');
+    window.location.replace('/views/home.html');
+  } else {
+    alert('Datos incorrectos');
+  }
+});
---
+++ js/register.js
@@ -0,0 +1,5 @@
+const registerForm = document.getElementById('form-container');
+
+registerForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+});
