bc26aedc97291cea6fe1f803cef87e47f717194f
Leonel Cordero <leonelrcordero22@gmail.com>
2025-11-16 15:40:37 -0300

    Se termino la logica del inicio de sesion y del registro

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
-const user = {
-  username: 'leonel',
-  password: 'admin',
-};
+const key = 'userSession';
 const loginForm = document.getElementById('form-container');
 
 loginForm.addEventListener('submit', function (event) {
   event.preventDefault();
   const userLogin = document.getElementById('user-input').value;
   const passLogin = document.getElementById('password-input').value;
-  if (user.username == userLogin && user.password == passLogin) {
-    alert('Datos correctos');
+  let userFound = false;
+
+  const usersLocalStorage = localStorage.getItem(key);
+  const users = JSON.parse(usersLocalStorage);
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username == userLogin && users[i].password == passLogin) {
+      userFound = true;
+      break;
+    }
+  }
+
+  if (userFound) {
     window.location.replace('/views/home.html');
   } else {
-    alert('Datos incorrectos');
+    alert(
+      'El usuario ingresado no se encuentra registrado o la contraseña es incorrecta.'
+    );
   }
 });
